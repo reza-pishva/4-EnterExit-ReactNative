86286fb504804a1ac03193ec690be5061542e74e
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-01-16 09:11:14 +0330

    Update ActivityIndicator3.js

--- app/components/ActivityIndicator3.js
+++ app/components/ActivityIndicator3.js
@@ -3,16 +3,11 @@ import LottieView from 'lottie-react-native';
 
 function ActivityIndicator3({visible='false'}) {
     if(!visible) return null;
-    // source={require('./assets/animations/51499-loading-check.json')}
-    // source={require('./assets/animations/50415-carrier-animation.json')}
-    // source={require('./assets/animations/45037-loading-blue-generic.json')}
-    // source={require('./assets/animations/33299-green-loading.json')}
     return(<LottieView 
         autoPlay
         loop
         source={require('./assets/animations/51499-loading-check.json')}
-      />);
-        
+      />);        
 
 }
 
